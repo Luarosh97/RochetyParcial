837c146a245b3ae4e502308ad0c2ea6fc8ef8558
luarosh97 <larturorocha@unicesar.edu.co>
2020-10-06 18:03:27 +0200

    Dandole

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{ClienteConsultaComponent}from'./credito/cliente-consulta/cliente-consulta.component';
+import{ClienteRegistroComponent}from'./credito/cliente-registro/cliente-registro.component';
+import{HomeComponent}from'./credito/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '/', component : HomeComponent},
+    { path: 'clienteConsulta', component:ClienteConsultaComponent },
+    { path: 'clienteRegistro', component: ClienteRegistroComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ClienteConsultaComponent } from './Credito/cliente-consulta/cliente-con
 import { ClienteRegistroComponent } from './Credito/cliente-registro/cliente-registro.component';
 import { FooterComponent } from './credito/footer/footer.component';
 import { NavmenuComponent } from './credito/navmenu/navmenu.component';
+import { HomeComponent } from './credito/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavmenuComponent } from './credito/navmenu/navmenu.component';
     ClienteConsultaComponent,
     ClienteRegistroComponent,
     FooterComponent,
-    NavmenuComponent
+    NavmenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
